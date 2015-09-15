3c8a0595b143234375394cbda03d3a7eaa57d5f6
Christine Cha <hello@christinecha.com>
2015-09-14 23:37:10 -0400

    Changed cache to false on design tool ajax call.

--- app/assets/javascripts/designtool.js
+++ app/assets/javascripts/designtool.js
@@ -144,6 +144,7 @@ $(document).ready(function(){
     var valuesToSubmit = $(this).serialize();
     $.ajax({
         type: "POST",
+        cache: false,
         url: $(this).attr('action'), //sumbits it to the given url of the form
         data: valuesToSubmit,
         dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
